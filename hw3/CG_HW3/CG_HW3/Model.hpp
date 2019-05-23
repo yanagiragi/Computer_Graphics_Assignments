687e8da1328dad2f9a730c82390598f26c8acc11
yanagiragi <qaz90473@gmail.com>
2019-05-23 23:10:44 +0800

    Update Model.hpp

--- hw3/CG_HW3/CG_HW3/Model.hpp
+++ hw3/CG_HW3/CG_HW3/Model.hpp
@@ -63,6 +63,9 @@ void LoadObj(char* filename)
 		glm::vec2 deltaUV1 = uv1 - uv0;
 		glm::vec2 deltaUV2 = uv2 - uv0;
 
+		// Check:
+		// http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-13-normal-mapping/
+		// http://ogldev.atspace.co.uk/www/tutorial26/tutorial26.html
 		float r = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV1.y * deltaUV2.x);
 		glm::vec3 tangent = (deltaPos1 * deltaUV2.y - deltaPos2 * deltaUV1.y)*r;
 		glm::vec3 bitangent = (deltaPos2 * deltaUV1.x - deltaPos1 * deltaUV2.x)*r;
